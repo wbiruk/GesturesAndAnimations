6e574833afef66b43a3289fe8a42381687192c21
Wiktor Biruk <wbiruk@wp.pl>
2015-07-27 10:42:00 +0200

    Initial Commit

---
+++ GesturesAndAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GesturesAndAnimations
+//
+//  Created by Wiktor Biruk on 27/07/15.
+//  Copyright (c) 2015 Wiktor Biruk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GesturesAndAnimations/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GesturesAndAnimations
+//
+//  Created by Wiktor Biruk on 27/07/15.
+//  Copyright (c) 2015 Wiktor Biruk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
